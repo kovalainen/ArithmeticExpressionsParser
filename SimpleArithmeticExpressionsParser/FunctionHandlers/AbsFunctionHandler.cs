c486c874f8e3189a77f0443d22a64c46c54e8445
Bohdan.Kovalevskyi <bogdan931013@gmail.com>
2023-07-22 22:51:02 +0300

    FunctionHandlers

---
+++ SimpleArithmeticExpressionsParser/FunctionHandlers/AbsFunctionHandler.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace SimpleArithmeticExpressionsParser.FunctionHandlers
+{
+    public class AbsFunctionHandler : OneArgumentFunctionHandlerBase
+    {
+        protected override string FunctionName => nameof(Math.Abs).ToLower();
+        
+        protected override double ApplyFunction(double value) => Math.Abs(value);
+    }
+}
---
+++ SimpleArithmeticExpressionsParser/FunctionHandlers/SqrtFunctionHandler.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace SimpleArithmeticExpressionsParser.FunctionHandlers
+{
+    public class SqrtFunctionHandler : OneArgumentFunctionHandlerBase
+    {
+        protected override string FunctionName => nameof(Math.Sqrt).ToLower();
+        
+        protected override double ApplyFunction(double value) => Math.Sqrt(value);
+    }
+}
